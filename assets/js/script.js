8bca0c10c75c0d6dafb4be7728731db372ccf39c
matt <matthewneverhart@gmail.com>
2018-09-15 21:49:25 -0500

    trying to put gyro stuff in html?

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,31 +5,31 @@ $(".testDiv").html("jquery works!");
 //var GyroNorm = require('gyronorm');
 
 
-var gn = new GyroNorm();
-
-gn.init().then(function(){
-  gn.start(function(data){
-    $(".testDiv").append("orientation absolute value? :"+data.do.absolute);
-    console.log("We got insite the callback of GyroNorm");
-    // Process:
-    // data.do.alpha	( deviceorientation event alpha value )
-    // data.do.beta		( deviceorientation event beta value )
-    // data.do.gamma	( deviceorientation event gamma value )
-    // data.do.absolute	( deviceorientation event absolute value )
-
-    // data.dm.x		( devicemotion event acceleration x value )
-    // data.dm.y		( devicemotion event acceleration y value )
-    // data.dm.z		( devicemotion event acceleration z value )
-
-    // data.dm.gx		( devicemotion event accelerationIncludingGravity x value )
-    // data.dm.gy		( devicemotion event accelerationIncludingGravity y value )
-    // data.dm.gz		( devicemotion event accelerationIncludingGravity z value )
-
-    // data.dm.alpha	( devicemotion event rotationRate alpha value )
-    // data.dm.beta		( devicemotion event rotationRate beta value )
-    // data.dm.gamma	( devicemotion event rotationRate gamma value )
-  });
-}).catch(function(e){
-  $(".testDiv").html("apparently deviceOrientation and DeviceMotion are not supported by your browser");
-  // Catch if the DeviceOrientation or DeviceMotion is not supported by the browser or device
-});
+// var gn = new GyroNorm();
+//
+// gn.init().then(function(){
+//   gn.start(function(data){
+//     $(".testDiv").append("orientation absolute value? :"+data.do.absolute);
+//     console.log("We got insite the callback of GyroNorm");
+//     // Process:
+//     // data.do.alpha	( deviceorientation event alpha value )
+//     // data.do.beta		( deviceorientation event beta value )
+//     // data.do.gamma	( deviceorientation event gamma value )
+//     // data.do.absolute	( deviceorientation event absolute value )
+//
+//     // data.dm.x		( devicemotion event acceleration x value )
+//     // data.dm.y		( devicemotion event acceleration y value )
+//     // data.dm.z		( devicemotion event acceleration z value )
+//
+//     // data.dm.gx		( devicemotion event accelerationIncludingGravity x value )
+//     // data.dm.gy		( devicemotion event accelerationIncludingGravity y value )
+//     // data.dm.gz		( devicemotion event accelerationIncludingGravity z value )
+//
+//     // data.dm.alpha	( devicemotion event rotationRate alpha value )
+//     // data.dm.beta		( devicemotion event rotationRate beta value )
+//     // data.dm.gamma	( devicemotion event rotationRate gamma value )
+//   });
+// }).catch(function(e){
+//   $(".testDiv").html("apparently deviceOrientation and DeviceMotion are not supported by your browser");
+//   // Catch if the DeviceOrientation or DeviceMotion is not supported by the browser or device
+// });
